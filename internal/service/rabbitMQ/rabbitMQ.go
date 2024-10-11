8341bf2a94b0cc68e33f005052afdfbe4f15fb95
San43zS <saniakovalevichenjoy@mail.ru>
2024-10-11 03:13:37 +0300

    rabbitMQ send

--- internal/service/rabbitMQ/rabbitMQ.go
+++ internal/service/rabbitMQ/rabbitMQ.go
@@ -1,25 +1,14 @@
 package rabbitMQ
 
 import (
+	"Lists-app/internal/service/rabbitMQ/config"
 	"context"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"time"
-
-	"Lists-app/internal/service/rabbitMQ/config"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-type RbbMQ interface {
-	Connect(config config.Config) error
-	Send() error
-}
-
-type RabbitMQ struct {
-	rbbMQ RbbMQ
-}
-
-func (r RabbitMQ) Connect(config config.Config) error {
+func Send(config config.Config, msg string) {
 	conn, err := amqp.Dial(config.Driver + config.URL)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -27,26 +16,32 @@ func (r RabbitMQ) Connect(config config.Config) error {
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
-
 	err = ch.ExchangeDeclare(
-		"logs_direct", // name
-		"direct",      // type
-		true,          // durable
-		false,         // auto-deleted
-		false,         // internal
-		false,         // no-wait
-		nil,           // arguments
+		"Rabbit", // name
+		"direct", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	return r.rbbMQ.Connect()
-}
+	body := msg
+	err = ch.PublishWithContext(ctx,
+		"Rabbit", // exchange
+		"yellow", // routing key
+		false,    // mandatory
+		false,    // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
 
-func (r RabbitMQ) Send() error {
-	return r.rbbMQ.Send()
 }
 
 func failOnError(err error, msg string) {
