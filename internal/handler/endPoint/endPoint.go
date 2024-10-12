bd6bb92c86c00097ce5f560bad5f586be7a6897a
San43zS <saniakovalevichenjoy@mail.ru>
2024-10-13 02:27:08 +0300

    gg

--- internal/handler/endPoint/endPoint.go
+++ internal/handler/endPoint/endPoint.go
@@ -1,1 +1,21 @@
 package endPoint
+
+import (
+	"Lists-app/internal/service"
+	"Lists-app/pkg/msgHandler"
+)
+
+type handler struct {
+	srv    service.Service
+	router msgHandler.MsgHandler
+}
+
+func New(srv service.Service) msgHandler.MsgHandler {
+	handler := &handler{
+		srv:    srv,
+		router: msgHandler.New(srv),
+	}
+
+	return handler.router
+
+}
